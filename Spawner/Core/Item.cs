8b20d3c02ecc6909989f116beec0a149403003b8
Kyle Brasenell <miner717@hotmail.com>
2023-06-24 18:15:12 +0100

    Update core item to remove fluidOverride

--- Spawner/Core/Item.cs
+++ Spawner/Core/Item.cs
@@ -12,13 +12,12 @@ namespace SpawnerTLD.Core
 		public GameObject item;
 		public Texture2D thumbnail;
 		public int conditionInt = 0;
-		public bool fluidOverride = false;
 		public int fuelMixes = 1;
 		public List<float> fuelValues = new List<float> { -1f };
 		public List<int> fuelTypeInts = new List<int> { -1 };
 		public Color color = new Color(255f / 255f, 255f / 255f, 255f / 255f);
 
-		public enum condition
+		public enum Condition
 		{
 			Random = -1,
 			Pristine,
