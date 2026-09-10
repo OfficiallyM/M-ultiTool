a858d83ec93d97a9739867583a69fb885046b154
unknown <miner717@hotmail.com>
2026-09-10 18:48:38 +0100

    feat: Convert styling to use Newtonsoft.Json, fix config color serialisation

---
+++ M-ultiTool/Extensions/ColorJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using UnityEngine;
+
+namespace MultiTool.Extensions
+{
+	internal class ColorJsonConverter : JsonConverter<Color>
+	{
+		public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
+		{
+			writer.WriteStartObject();
+			writer.WritePropertyName("r"); writer.WriteValue(value.r);
+			writer.WritePropertyName("g"); writer.WriteValue(value.g);
+			writer.WritePropertyName("b"); writer.WriteValue(value.b);
+			writer.WritePropertyName("a"); writer.WriteValue(value.a);
+			writer.WriteEndObject();
+		}
+
+		public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			var obj = JObject.Load(reader);
+			return new Color(
+				obj["r"]?.Value<float>() ?? 0f,
+				obj["g"]?.Value<float>() ?? 0f,
+				obj["b"]?.Value<float>() ?? 0f,
+				obj["a"]?.Value<float>() ?? 1f);
+		}
+	}
+}
